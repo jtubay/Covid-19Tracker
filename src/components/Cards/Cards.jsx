fbe7c77dd50451ee23e320d0cfe7c8328a945259
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-08 11:37:16 -0400

    adding props

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,17 +2,37 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
 import styles from './Cards.module.css'
-const Cards = (props) =>{
-    console.log(props)
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) =>{
+  
+    if(!confirmed) {
+        return 'loading.....'
+    }
+
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">Real Data</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
                         <Typography color="textSecondary">Real Data</Typography>
                         <Typography variant="body2">Num of Active cases</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">Num of Recoveries</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Death</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">Num of deaths</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
