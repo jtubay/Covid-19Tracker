f871a81eac465a8f57904517321511bb464a31d3
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-07 22:45:22 -0400

    setting up component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
+
+
+import { Cards, Chart, CountryPicker }  from './components'
+
 class App extends Component {
     render() {
         return(
         <div>
-            <h1>App</h1>
-
+            <Cards />
+            <CountryPicker />
+            <Chart />
         </div>
         )
     }
