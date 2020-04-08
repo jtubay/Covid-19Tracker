aa676b98a9e30b19a097778fb602ed275ac3a5c5
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-08 11:24:04 -0400

    adding card from material

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
+import styles from './Cards.module.css'
 const Cards = (props) =>{
     console.log(props)
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">Num of Active cases</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
 export default Cards;
+
