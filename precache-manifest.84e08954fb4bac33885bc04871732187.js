7cf3a9290e5f3a1f35198f77b4b6b77677f9f959
Jean Tubay <jean.tubay.92@gmail.com>
2020-04-08 21:15:47 -0400

    Updates

---
+++ precache-manifest.84e08954fb4bac33885bc04871732187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a544c0cf02138ecdd4123a2c824841be",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "b1493e31f3e89e54a079",
+    "url": "/Covid19Tracker/static/css/main.76e9a63e.chunk.css"
+  },
+  {
+    "revision": "3262baef9db6aaebbd14",
+    "url": "/Covid19Tracker/static/js/2.8a90705c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.8a90705c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1493e31f3e89e54a079",
+    "url": "/Covid19Tracker/static/js/main.61164dbd.chunk.js"
+  },
+  {
+    "revision": "643a26a0ba143a9d5994",
+    "url": "/Covid19Tracker/static/js/runtime-main.79b4259d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19Tracker/static/media/covid.d7265326.png"
+  }
+]);
